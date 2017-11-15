c47f5e707d8b8daf72c33f29fd4a8ff8baee36f2
Baruch Velez <baruch.velez@mcmakler.de>
2017-11-15 11:26:25 +0100

    feature(global): v0.0.1-beta.10

--- packages/design-guide/server.js
+++
@@ -1,8 +0,0 @@
-const server = require('pushstate-server');
-
-const port = process.env.PORT || 9002;
-
-server.start({
-  port,
-  directory: './dist',
-});
--- packages/design-guide/src/components/App/MiniMapPage/snippets/minimap.snippet.js
+++
@@ -1,8 +0,0 @@
-export const noMarker = `<MiniMap
-  isMarkerShownn={false}
-  center={{ lat: 52.529784, lng: 13.401378 }}
-/>`;
-export const marker = `<MiniMap
-  isMarkerShown
-  center={{ lat: 52.529784, lng: 13.401378 }}
-/>`;
