91358d323ab7319298e7f4f6b428ade38528be77
Kathrin Holzmann <kathrin.holzmann@mcmakler.de>
2017-11-24 15:31:35 +0100

    chore(Storybook): Adds custom style to storybook preview

--- packages/components-react/.storybook/config.js
+++ packages/components-react/.storybook/config.js
@@ -1,12 +1,19 @@
 import React from 'react';  //eslint-disable-line
-import { configure, setAddon } from '@storybook/react';
+import { configure, setAddon, addDecorator } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
-
+import './_custom_style.scss';
 setAddon(infoAddon);
+
+addDecorator(story => (
+  <div className='storycontainer'>
+    {story()}
+  </div>
+));
 
 function loadStories() {
   const req = require.context('../stories', true, /\.js$/);
   req.keys().forEach(filename => req(filename));
 }
+
 
 configure(loadStories, module);
