8da151d6967fb9a166acfb2c31d242bb883a81d1
Baruch Velez <baruch.velez@mcmakler.de>
2018-03-16 12:35:56 -0400

    chore(storybook): Adjust RadioGroup story to better display multiple

--- packages/components-react/stories/RadioGroup.js
+++ packages/components-react/stories/RadioGroup.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { action, storiesOf } from '@storybook/react';
 import { RadioGroup } from '../src';
 
+const multiple = [
+  {
+    buttons: [
+      { value: 'Foo', labelText: 'Foo Label' },
+      { value: 'Bar', labelText: 'Bar Label' },
+    ],
+  },
+  {
+    buttons: [
+      { value: 'Foo', labelText: 'Foo Label' },
+      { value: 'Bar', labelText: 'Bar Label' },
+    ],
+  },
+];
+
 storiesOf('RadioGroup', module)
   .addWithInfo(
-    'enabled',
+    'basic',
     '',
     () => (
       <RadioGroup
@@ -14,5 +29,18 @@ storiesOf('RadioGroup', module)
         ]}
         onChange={action('onChange')}
       />
+    ),
+  )
+  .addWithInfo(
+    'multiple',
+    `
+      Note: when having multiple RadioGroups in one page, each RadioGroup must have a unique 'id' property
+    `,
+    () => (
+      <div>
+        { multiple.map((rg, i) => (
+          <RadioGroup id={i} {...rg} onChange={action('onChange')} />
+        )) }
+      </div>
     ),
   );
