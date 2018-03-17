c60cd879322037c6005f7801e1f6c52c8f27b508
Baruch Velez <baruch.velez@mcmakler.de>
2018-03-17 18:46:09 -0400

    chore(BaseButton): adjust test to be example

--- packages/components-react/src/components/BaseButton/BaseButton.spec.js
+++ packages/components-react/src/components/BaseButton/BaseButton.spec.js
@@ -2,33 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import BaseButton from './BaseButton';
 
-test('renders', () => {
-  const label = 'test';
+test('Correctly renders.', () => {
+  const label = 'Render Test';
   const tree = renderer.create(<BaseButton label={label} />).toJSON();
 
   expect(tree).toMatchSnapshot();
-});
-
-test('should be disabled', () => {
-  const label = 'test';
-  const tree = renderer.create(
-    <BaseButton
-      disabled={true}
-      label={label}
-    />
-  ).toJSON();
-
-  expect(tree).toMatchSnapshot();
-  expect(tree.props).toHaveProperty('disabled', true);
-});
-
-test('must have correct label', () => {
-  const label = 'test';
-  const tree = renderer.create(
-    <BaseButton
-      label={label}
-    />
-  ).toJSON();
-
-  expect(tree.children).toEqual(expect.arrayContaining([label]));
 });
